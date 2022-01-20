dfc2a140be4556e814a62dd8d8cda95fb3fc5750
RadRudransh72 <98090431+RadRudransh72@users.noreply.github.com>
2022-01-20 16:00:15 +0530

    uploaded all the files.
    initial commit.

---
+++ sketch.js
@@ -0,0 +1,36 @@
+var ball ;
+
+function setup() {
+  createCanvas(400,400);
+  ball = createSprite(200,200,20,20);
+}
+
+function draw() 
+{
+  background(30);
+
+  if(keyDown("right"))
+  {
+    ball.x = ball.x + 3;
+  }
+
+  if(keyDown("left"))
+  {
+    ball.x = ball.x - 3;
+  }
+  if(keyDown("up"))
+  {
+    ball.y = ball.y - 3;
+  }
+  if(keyDown("down"))
+  {
+    ball.y = ball.y + 3;
+  }
+
+
+  drawSprites();
+}
+
+
+
+
